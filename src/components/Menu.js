32502e5c36c49cc40cfd2f5f075e8041b5c4dca1
duvan aponte ramirez <duvanaponteramirez@gmail.com>
2022-05-14 00:32:41 -0500

    menu

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -14,7 +14,7 @@ const Menu = ({menu, handlerid}) => {
     <>
         <nav className="navbar navbar-expand-lg navbar-light bg-light menuNavbar">
             <div className="container-fluid">
-                <a className="navbar-brand" >Navbar</a>
+                <a href="#" className="navbar-brand"> Nabvar </a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
                 <span className="navbar-toggler-icon"></span>
                 </button>
